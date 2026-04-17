ada352e426af1e3d862eb31f028cd646783fd564
Roman <59706096+krikinlive@users.noreply.github.com>
2026-04-17 15:13:45 -0700

    Lab 3
    new changes for lab3

---
+++ global.js
@@ -0,0 +1,72 @@
+console.log("IT'S ALIVE!");
+
+function $$(selector, context = document) {
+    return Array.from(context.querySelectorAll(selector));
+}
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"
+    : "/portfolio_lab_1_dsc106/";
+
+// STEP 3
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/krikinlive', title: 'GitHub' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    if (!url.startsWith('http')) {
+        url = BASE_PATH + url;
+    }
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
+
+    nav.append(a);
+}
+
+//STEP 4
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `<label class="color-scheme">
+        Theme:
+        <select>
+            <option value="light dark">Automatic</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+        </select>
+    </label>`
+);
+
+const select = document.querySelector('.color-scheme select');
+
+if ('colorScheme' in localStorage) {
+    setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener('input', function (event) {
+    setColorScheme(event.target.value);
+    localStorage.colorScheme = event.target.value;
+});
+
+function setColorScheme(scheme) {
+    document.documentElement.style.setProperty('color-scheme', scheme);
+    select.value = scheme;
+}
