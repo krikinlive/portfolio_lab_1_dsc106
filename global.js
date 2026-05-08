6fa831c9d35e9feababf43573f377e916b024195
Roman <59706096+krikinlive@users.noreply.github.com>
2026-05-08 15:03:14 -0700

    Lab 6 changes
    Lab Final Version

--- global.js
+++ global.js
@@ -45,6 +45,7 @@ let pages = [
     { url: '', title: 'Home' },
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact' },
+    { url: 'meta/', title: 'Meta' },
     { url: 'https://github.com/krikinlive', title: 'GitHub' },
 ];
 
